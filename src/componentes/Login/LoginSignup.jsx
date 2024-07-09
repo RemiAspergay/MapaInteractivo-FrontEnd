edaeb39bf4f8c9f4aa67ca186809da59a15b0afb
Paula <paula.vargas.cist@gmail.com>
2024-07-09 15:29:47 -0600

    Login

---
+++ src/componentes/Login/LoginSignup.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import "./LoginSignup.css";
+
+const LoginSignup = () => {
+  const [action, setAction] = useState("Sign Up");
+  return (
+    <div className="container">
+      <div className="header">
+        <div className="text">Regístrate</div>
+        <div className="underline"></div>
+      </div>
+      <div className="inputs">
+        {action === "Login" ? (
+          <div></div>
+        ) : (
+          <div className="input">
+            <input type="text" placeholder="Nombre" />
+          </div>
+        )}
+
+        <div className="input">
+          <input type="email" placeholder="correo" />
+        </div>
+        <div className="input">
+          <input type="password" placeholder="contraseña" />
+        </div>
+
+        <div className="submit-container">
+          <div
+            className={action === "Sign Up" ? "submit pink" : "submit"}
+            onClick={() => {
+              setAction("Sign Up");
+            }}
+          >
+            Regístrate
+          </div>
+          <div
+            className={action === "Login" ? "submit pink" : "submit"}
+            onClick={() => {
+              setAction("Login");
+            }}
+          >
+            Inicia Sesión
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginSignup;
