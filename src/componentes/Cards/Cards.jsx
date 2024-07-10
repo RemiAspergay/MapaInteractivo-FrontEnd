ad33ebb305e3269ba1459f0bb625946bb0d2d251
haals9000 <hal.almonacid.lopez@gmail.com>
2024-07-10 12:19:25 -0400

    ajustar mapa

--- src/componentes/Cards/Cards.jsx
+++ src/componentes/Cards/Cards.jsx
@@ -18,10 +18,7 @@ function Cards({ data }) {
   const navigate = useNavigate();
   return (
     <>
-      <Container sx={{ my: "20px" }}>
-        <Typography variant="h1" sx={{ mt: "100px" }}>
-        </Typography>
-        <Grid container spacing={2} sx={{ mb: "20px" }}>
+        <Grid container sx={{ mb: "20px", my: "auto" }}>
           {data.map((region, index) => (
             <Grid
               key={index}
@@ -32,23 +29,24 @@ function Cards({ data }) {
               lg={12}
               onClick={() => handleDivClick(region.id)}
             >
-              <Card sx={{ cursor: "pointer", width: "300px", margin: "auto", border: "0", boxShadow:"0"}}>
+              <Card sx={{ cursor: "pointer", width: "300px", margin: "auto", border: "0", boxShadow:"0", background:"transparent", marginTop: region.marginTop, marginBottom: region.marginBottom, marginLeft: region.marginLeft, marginRight: region.marginRight
+              }}>
                 <CardMedia
                   component="img"
                   alt={region.nombre}
                   src={region.imagen}
                 />
-                <CardContent >
-                  <Typography variant="h6" >{region.nombre}</Typography>
+                {/* <CardContent >
+                  <Typography variant="h6" sx={{textAlign: "right"}}>{region.nombre}</Typography>
                 </CardContent>
-                <Link to={`/Region/${region.id}`}></Link>
+                <Link to={`/Region/${region.id}`}></Link> */}
               </Card>
             </Grid>
           ))}
         </Grid>
-      </Container>
     </>
   );
 }
 
 export default Cards;
+
--- src/componentes/assets/all_region.js
+++ src/componentes/assets/all_region.js
@@ -21,96 +21,160 @@ let all_region = [
     nombre:
       "Región de Arica y Parinacota",
     imagen: p1_img,
+    marginTop: 0,
+    marginBottom: 0,
+    marginLeft: "150px",
+    marginRight: 0
   },
   {
     id: 2,
     nombre:
       "Región de Tarapacá",
     imagen: p2_img,
+    marginTop: "-70px",
+    marginBottom: 0,
+    marginLeft: "160px",
+    marginRight: 0
   },
   {
     id: 3,
     nombre:
       "Región de Antofagasta",
     imagen: p3_img,
+    marginTop: "-65px",
+    marginBottom: 0,
+    marginLeft: "185px",
+    marginRight: 0
   },
   {
     id: 4,
     nombre:
       "Región de Atacama",
     imagen: p4_img,
+    marginTop: "-65px",
+    marginBottom: 0,
+    marginLeft: "140px",
+    marginRight: 0
   },
   {
     id: 5,
     nombre:
       "Región de Coquimbo",
     imagen: p5_img,
+    marginTop: "-20px",
+    marginBottom: 0,
+    marginLeft: "110px",
+    marginRight: 0
   },
   {
     id: 6,
     nombre:
       "Región de Valparaíso",
     imagen: p6_img,
+    marginTop: "-50px",
+    marginBottom: 0,
+    marginLeft: "120px",
+    marginRight: 0
   },
   {
     id: 7,
     nombre:
       "Región Metropolitana de Santiago",
     imagen: p7_img,
+    marginTop: "-155px",
+    marginBottom: 0,
+    marginLeft: "150px",
+    marginRight: 0
   },
   {
     id: 8,
     nombre:
       "Región del Libertador General Bernardo O'Higgins",
     imagen: p8_img,
+    marginTop: "-124px",
+    marginBottom: 0,
+    marginLeft: "130px",
+    marginRight: 0
   },
   {
     id: 9,
     nombre:
       "Región del Maule",
     imagen: p9_img,
+    marginTop: "-110px",
+    marginBottom: 0,
+    marginLeft: "110px",
+    marginRight: 0
   },
   {
     id: 10,
     nombre:
       "Región de Ñuble",
     imagen: p10_img,
+    marginTop: "-165px",
+    marginBottom: 0,
+    marginLeft: "90px",
+    marginRight: 0
   },
   {
     id: 11,
     nombre:
       "Región del Biobío",
     imagen: p11_img,
+    marginTop: "-130px",
+    marginBottom: 0,
+    marginLeft: "60px",
+    marginRight: 0
   },
   {
     id: 12,
     nombre:
       "Región de La Araucanía",
     imagen: p12_img,
+    marginTop: "-140px",
+    marginBottom: 0,
+    marginLeft: "70px",
+    marginRight: 0
   },
   {
     id: 13,
     nombre:
       "Región de Los Ríos",
     imagen: p13_img,
+    marginTop: "-100px",
+    marginBottom: 0,
+    marginLeft: "50px",
+    marginRight: 0
   },
   {
     id: 14,
     nombre:
       "Región de Los Lagos",
     imagen: p14_img,
+    marginTop: "-70px",
+    marginBottom: 0,
+    marginLeft: "30px",
+    marginRight: 0
   },
   {
     id: 15,
     nombre:
       "Región de Aysén del General Carlos Ibáñez del Campo",
     imagen: p15_img,
+    marginTop: "-20px",
+    marginBottom: 0,
+    marginLeft: "35px",
+    marginRight: 0
   },
   {
     id: 16,
     nombre:
       "Región de Magallanes y de la Antártica Chilena",
     imagen: p16_img,
+    marginTop: "-45px",
+    marginBottom: 0,
+    marginLeft: "50px",
+    marginRight: 0
   },
 ];
 
